0af4fc700cdee23e8b748a3f9fcc32f4c4d29ed3
Vinicius Montes <viniciusmontes@outlook.com.br>
2024-04-29 15:43:33 -0300

    Spinner (Loading)

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -11,7 +11,7 @@ import { CoursesService } from './../services/courses.service';
 })
 export class CoursesComponent implements OnInit {
 
-  courses : Observable<Course[]>;
+  courses$ : Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
   // coursesService : CoursesService;
@@ -19,7 +19,7 @@ export class CoursesComponent implements OnInit {
   constructor(private coursesService : CoursesService ) {
     // this.courses = [];
     // this.coursesService = new CoursesService();
-    this.courses = this.coursesService.list();
+    this.courses$ = this.coursesService.list();
   }
 
   ngOnInit(): void {
