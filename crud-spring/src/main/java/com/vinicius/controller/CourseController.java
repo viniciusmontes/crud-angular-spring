3181adab2563443d1c794a6f2b15bde77577f9df
Vinicius Montes <viniciusmontes@outlook.com.br>
2024-05-02 10:37:07 -0300

    Form: Spring API: Create Course (HTTP POST)

--- crud-spring/src/main/java/com/vinicius/controller/CourseController.java
+++ crud-spring/src/main/java/com/vinicius/controller/CourseController.java
@@ -2,9 +2,13 @@ package com.vinicius.controller;
 
 import java.util.List;
 
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vinicius.model.Course;
@@ -22,6 +26,12 @@ public class CourseController {
     @GetMapping
     public List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return courseRepository.save(course);
     }
 
 }
