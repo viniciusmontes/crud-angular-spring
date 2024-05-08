c092b89e5b48497aa1aa3fbdb5dd1af3009f62b8
Vinicius Montes <viniciusmontes@outlook.com.br>
2024-05-08 11:17:11 -0300

    Update Course: Saving the Form

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -5,28 +5,38 @@ import { delay, first, tap } from 'rxjs/operators';
 import { Course } from '../model/course';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CoursesService {
-
-
   private readonly API = 'api/courses';
-  constructor(private httpClient : HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   list() {
-    return this.httpClient.get<Course[]>(this.API)
-    .pipe(
+    return this.httpClient.get<Course[]>(this.API).pipe(
       first(),
       delay(1000),
-      tap(courses => console.log(courses))
+      tap((courses) => console.log(courses))
     );
   }
 
-  loadById(id : string) {
+  loadById(id: string) {
     return this.httpClient.get<Course>(`${this.API}/${id}`);
   }
 
   save(record: Partial<Course>) {
+    if (record._id) {
+      return this.update(record);
+    }
+    return this.create(record);
+  }
+
+  private create(record: Partial<Course>) {
     return this.httpClient.post<Course>(this.API, record).pipe(first());
+  }
+
+  private update(record: Partial<Course>) {
+    return this.httpClient
+      .put<Course>(`${this.API}/${record._id}`, record)
+      .pipe(first());
   }
 }
