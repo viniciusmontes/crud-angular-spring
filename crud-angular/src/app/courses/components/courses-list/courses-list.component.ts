22aea5b84fbf473211b55f885ce9b124981fd186
Vinicius Montes <viniciusmontes@outlook.com.br>
2024-05-08 12:19:52 -0300

    Remove Course

--- crud-angular/src/app/courses/components/courses-list/courses-list.component.ts
+++ crud-angular/src/app/courses/components/courses-list/courses-list.component.ts
@@ -5,27 +5,29 @@ import { Course } from '../../model/course';
 @Component({
   selector: 'app-courses-list',
   templateUrl: './courses-list.component.html',
-  styleUrls: ['./courses-list.component.scss']
+  styleUrls: ['./courses-list.component.scss'],
 })
 export class CoursesListComponent implements OnInit {
-
-  @Input() courses : Course[] = [];
+  @Input() courses: Course[] = [];
   @Output() add = new EventEmitter(false);
   @Output() edit = new EventEmitter(false);
+  @Output() remove = new EventEmitter(false);
 
   readonly displayedColumns = ['name', 'category', 'actions'];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onAdd() {
     this.add.emit(true);
   }
 
-  onEdit(course : Course) {
+  onEdit(course: Course) {
     this.edit.emit(course);
   }
 
+  onDelete(course: Course) {
+    this.remove.emit(course);
+  }
 }
--- crud-angular/src/app/courses/containers/courses/courses.component.ts
+++ crud-angular/src/app/courses/containers/courses/courses.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
@@ -8,15 +9,13 @@ import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/err
 import { Course } from '../../model/course';
 import { CoursesService } from '../../services/courses.service';
 
-
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
-  courses$: Observable<Course[]>;
-
+  courses$: Observable<Course[]> | null = null;
 
   // coursesService : CoursesService;
 
@@ -24,10 +23,15 @@ export class CoursesComponent implements OnInit {
     private coursesService: CoursesService,
     public dialog: MatDialog,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private snackBar: MatSnackBar
   ) {
     // this.courses = [];
     // this.coursesService = new CoursesService();
+    this.refresh();
+  }
+
+  refresh() {
     this.courses$ = this.coursesService.list().pipe(
       catchError((error) => {
         this.onError('Erro ao carregar cursos.');
@@ -45,11 +49,24 @@ export class CoursesComponent implements OnInit {
   ngOnInit(): void {}
 
   onAdd() {
-    this.router.navigate(['new'], {relativeTo: this.route});
+    this.router.navigate(['new'], { relativeTo: this.route });
   }
 
-  onEdit(course : Course) {
-    this.router.navigate(['edit', course._id], {relativeTo: this.route});
+  onEdit(course: Course) {
+    this.router.navigate(['edit', course._id], { relativeTo: this.route });
   }
 
+  onRemove(course: Course) {
+    this.coursesService.remove(course._id).subscribe(
+      () => {
+        this.refresh();
+        this.snackBar.open('Curso removido com sucesso!', 'X', {
+          duration: 5000,
+          verticalPosition: 'top',
+          horizontalPosition: 'center',
+        });
+      },
+      () => this.onError('Erro ao tentar remover o curso.')
+    );
+  }
 }
