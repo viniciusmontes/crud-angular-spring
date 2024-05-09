bab53e3a9dba7ef466c6e1d7f3a0a003e99ce677
Vinicius Montes <viniciusmontes@outlook.com.br>
2024-05-09 16:47:40 -0300

    Handling Exceptions with Controller Advice

---
+++ crud-spring/src/main/java/com/vinicius/exception/RecordNotFoundException.java
@@ -0,0 +1,9 @@
+package com.vinicius.exception;
+
+public class RecordNotFoundException extends RuntimeException {
+
+    public RecordNotFoundException(Long id) {
+        super("Registro não encontrado com o id: " + id);
+    }
+    
+}
