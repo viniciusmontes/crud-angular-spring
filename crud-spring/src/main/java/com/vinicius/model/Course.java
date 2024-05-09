7ce6a729c704b22ed8060e194eddd6411db244a9
Vinicius Montes <viniciusmontes@outlook.com.br>
2024-05-09 11:12:59 -0300

    API validations with Java Bean Validation

--- crud-spring/src/main/java/com/vinicius/model/Course.java
+++ crud-spring/src/main/java/com/vinicius/model/Course.java
@@ -1,5 +1,11 @@
 package com.vinicius.model;
 
+
+
+
+
+import org.hibernate.validator.constraints.Length;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
@@ -7,6 +13,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -18,9 +27,15 @@ public class Course {
     @JsonProperty("_id")
     private Long id;
 
+    @NotBlank
+    @Length(min =5, max = 100)
+    @NotNull
     @Column(length = 200, nullable = false)
     private String name;
 
+    @NotBlank
+    @Length(max = 100)
+    @Pattern(regexp = "Back-end|Front-end")
     @Column(length = 10, nullable = false)
     private String category;
 
