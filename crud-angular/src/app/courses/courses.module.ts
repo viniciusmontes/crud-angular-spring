1b89c16025010210e8153568a1ab8115b5d4fb2b
Vinicius Montes <viniciusmontes@outlook.com.br>
2024-05-20 10:55:23 -0300

    Migration to standalone component - removal of modules

--- crud-angular/src/app/courses/courses.module.ts
+++ crud-angular/src/app/courses/courses.module.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { AppMaterialModule } from '../shared/app-material/app-material.module';
-import { SharedModule } from '../shared/shared.module';
+
+
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './containers/courses/courses.component';
 import { CourseFormComponent } from './containers/course-form/course-form.component';
@@ -13,14 +13,12 @@ import { CoursesListComponent } from './components/courses-list/courses-list.com
 
 @NgModule({
     imports: [
-        CommonModule,
-        CoursesRoutingModule,
-        AppMaterialModule,
-        SharedModule,
-        ReactiveFormsModule,
-        CoursesComponent,
-        CourseFormComponent,
-        CoursesListComponent
-    ]
+    CommonModule,
+    CoursesRoutingModule,
+    ReactiveFormsModule,
+    CoursesComponent,
+    CourseFormComponent,
+    CoursesListComponent
+]
 })
 export class CoursesModule { }
